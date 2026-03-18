21e946674a367ac012b9313292b1aabc299db992
tontacchi <andy.tonthat14@gmail.com>
2026-03-18 08:22:06 -0700

    chore: entry package declarations

--- internal/entry/input.go
+++ internal/entry/input.go
@@ -6,3 +6,24 @@ import (
 	"os"
 	"strings"
 )
+
+
+// MakeFromKeyboard reads user input from stdin, returns a new Entry
+func MakeFromKeyboard() Entry {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Title: ")
+	title, _ := reader.ReadString('\n')
+	title = strings.TrimSpace(title)
+
+	fmt.Print("Activity: ")
+	activity, _ := reader.ReadString('\n')
+	activity = strings.TrimSpace(activity)
+
+	return Entry{
+		Title: title,
+		Datetime: MakeTimestamp(),
+		Activity: activity,
+	}
+}
+
