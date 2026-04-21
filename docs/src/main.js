ed4bf48594ac7797a733d483513352fe5e72437d
tontacchi <andy.tonthat14@gmail.com>
2026-04-21 16:14:45 -0700

    feat: no white flash, fade-in animation for components

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,12 +1,15 @@
-import { $, listen } from "./dom.js";
+import { $, listen, removeClass } from "./dom.js";
 import { loadEntriesFromIndex } from "./data/loadEntries.js";
 import { renderEntries } from "./render/renderEntries.js";
 
 const root = $("activity");
+const app = $("app");
 
 async function main() {
 	const entries = await loadEntriesFromIndex("./data/index.json");
 	renderEntries(entries, root);
+
+	removeClass(app, "opacity-0");
 }
 
 
